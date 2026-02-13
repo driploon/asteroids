df995ce56d06bc20074210b8eb1ddff25135cbc3
driploon <driploon@proton.me>
2026-02-13 14:01:07 -0800

    creates player and starts movement commands

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import pygame
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    pygame_version = pygame.version.ver
-    print(f"Starting Asteroids with pygame version: {pygame_version}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    fps = pygame.time.Clock()
+    dt = 0
+
+
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = fps.tick(60) / 1000
 
 
 if __name__ == "__main__":
